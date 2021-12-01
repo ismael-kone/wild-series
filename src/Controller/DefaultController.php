fc4c8fa3070c031e4c5bc84413ff49714f8d2e54
Ismael-Kone <ismaelprokone@gmail.com>
2021-12-01 12:39:59 +0100

    added navbar, bootstrap and image

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
- * @Route("/default/", name="app_index")
+ * @Route("/", name="app_index")
  */
 public function index(): Response
 {
